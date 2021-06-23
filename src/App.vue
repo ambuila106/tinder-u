bd5efbe51fb3a9608adbdced5004d1469cf0d69f
ambuila106 <brandon.ambuila@unillanos.edu.co>
2021-06-23 07:00:56 -0500

    added vue tinder and implemented

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,114 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1 style="color: white; text-align: center; margin-bottom: 3rem;">Rela Universitario (villavicencio)</h1>
+    <Tinder class="tinder" key-name="id" :queue.sync="queue" :offset-y="10" @submit="onSubmit">
+      <template slot-scope="scope">
+        <div
+          class="pic"
+          :style="{
+            'background-image': `url(${scope.data.id})`
+          }"
+        />
+      </template>
+      <img class="like-pointer" slot="like" src="./assets/like-txt.png">
+      <img class="nope-pointer" slot="nope" src="./assets/nope-txt.png">
+      <img class="super-pointer" slot="super" src="./assets/super-txt.png">
+    </Tinder>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Tinder from "vue-tinder";
+import source from "./bing"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: { Tinder },
+  data: () => ({
+    queue: [],
+    offset: 0,
+  }),
+  created() {
+    this.mock();
+  },
+  methods: {
+    mock(count = 5) {
+      const list = [];
+      for (let i = 0; i < count; i++) {
+        list.push({ id: source[this.offset] });
+        this.offset++;
+      }
+      this.queue = this.queue.concat(list);
+    },
+    onSubmit() {
+      if (this.queue.length < 3) {
+        this.mock();
+      }
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html,
+body {
+  height: 100%;
+}
+
+body {
+  margin: 0;
+  background-color: #20262e;
+  overflow: hidden;
+}
+
+#app .vue-tinder {
+  position: absolute;
+  z-index: 1;
+  left: 0;
+  right: 0;
+  top: 23px;
+  bottom: 46px;
+  margin: auto;
+  min-width: 300px;
+  max-width: 355px;
+}
+
+.nope-pointer,
+.like-pointer {
+  position: absolute;
+  z-index: 1;
+  top: 20px;
+  width: 150px;
+  height: 150px;
+}
+
+.nope-pointer {
+  right: 10px;
+}
+
+.like-pointer {
+  left: 10px;
+}
+
+.super-pointer {
+  position: absolute;
+  z-index: 1;
+  bottom: 80px;
+  left: 0;
+  right: 0;
+  margin: auto;
+  width: 112px;
+  height: 78px;
+}
+
+.tinder {
+    margin-top: 3rem !important;
+}
+
+.pic {
+  width: 100%;
+  height: 100%;
+  background-size: cover;
+  background-position: center;
 }
 </style>
---
+++ src/bing.js
@@ -0,0 +1,5 @@
+export default [
+  'https://i.ibb.co/BPRg77r/154480838-1364334313922542-2149194858239639778-n.jpg',
+  'https://media.sf-converter.com/get?uri=https%3A%2F%2Finstagram.fiev21-1.fna.fbcdn.net%2Fv%2Ft51.2885-15%2Fe35%2Fp1080x1080%2F172153007_200657474906739_8928622783602050151_n.jpg%3Ftp%3D1%26_nc_ht%3Dinstagram.fiev21-1.fna.fbcdn.net%26_nc_cat%3D103%26_nc_ohc%3DG15qMLDIzZ0AX8dY2D4%26edm%3DAJBgZrYBAAAA%26ccb%3D7-4%26oh%3D603fe422902a91e24e0aa42484d50810%26oe%3D60D6C587%26_nc_sid%3D78c662',
+  'https://i.ibb.co/1rwqt9K/maryanntl.jpg'
+  ];
